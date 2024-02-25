56e04cc2cf9bb53ac13cae536fe7ef6689cb0316
Nathan Hartojo <nathanhartojo@gmail.com>
2024-02-25 01:03:26 -0600

    current prog

--- components/Friends.js
+++ components/Friends.js
@@ -1,9 +1,103 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { Image } from "expo-image";
+
+const listofUsers = [
+    {
+        name: "Nathan",
+        profilePic:
+            "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
+        region: "USA",
+    },
+    {
+        name: "John",
+        profilePic: "https://avatar.iran.liara.run/public/boy?username=Ash",
+        region: "USA",
+    },
+    {
+        name: "Sally",
+        profilePic:
+            "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
+        region: "USA",
+    },
+    {
+        name: "Jane",
+        profilePic: "https://avatar.iran.liara.run/public/boy?username=Ash",
+        region: "USA",
+    },
+    {
+        name: "Sue",
+        profilePic:
+            "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
+        region: "Mexico",
+    },
+    {
+        name: "Bob",
+        profilePic: "https://avatar.iran.liara.run/public/boy?username=Ash",
+        region: "Mexico",
+    },
+];
 
 export default function Friends() {
     return (
-        <View>
-            <Text>We made it to the friends</Text>
-        </View>
+        <FlatList
+            data={listofUsers}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+                <View style={styles.card}>
+                    <Image
+                        style={styles.image}
+                        source={{ uri: item.profilePic }}
+                    />
+                    <View style={styles.textContainer}>
+                        <Text style={styles.name}>{item.name}</Text>
+                        <Text style={styles.region}>
+                            {item.region || "Unknown"}
+                        </Text>
+                    </View>
+                </View>
+            )}
+            contentContainerStyle={styles.container}
+        />
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingVertical: 20,
+    },
+    card: {
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: "#f9f9f9",
+        borderRadius: 10,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 6,
+        elevation: 4,
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        marginVertical: 8,
+        marginHorizontal: 20,
+    },
+    textContainer: {
+        marginLeft: 10,
+    },
+    name: {
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+    region: {
+        fontSize: 14,
+        color: "#666",
+        marginTop: 4,
+    },
+    image: {
+        width: 70,
+        height: 70,
+        borderRadius: 35,
+    },
+});
--- components/Leaderboard.js
+++ components/Leaderboard.js
@@ -1,4 +1,32 @@
 import { View, Text } from "react-native";
+// remove all profilePic fields, replace with points field
+// add a points field to each user object
+const listofUsers = [
+    {
+        name: "Nathan",
+        points: 100,
+    },
+    {
+        name: "John",
+        points: 90,
+    },
+    {
+        name: "Sally",
+        points: 80,
+    },
+    {
+        name: "Jane",
+        points: 70,
+    },
+    {
+        name: "Sue",
+        points: 60,
+    },
+    {
+        name: "Bob",
+        points: 50,
+    },
+];
 
 export default function Leaderboard() {
     return (
