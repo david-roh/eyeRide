bb7e36d91404e5c72f9f9ccf4d4a0766ea1c77ae
Nathan Hartojo <nathanhartojo@gmail.com>
2024-02-24 19:08:17 -0600

    current layout

--- components/CameraDrowsy.js
+++ components/CameraDrowsy.js
@@ -1,9 +1,34 @@
-import { View, Text } from "react-native";
+import React from "react";
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function CameraDrowsy() {
+    const navigation = useNavigation();
+
     return (
-        <View>
-            <Text>We made it</Text>
+        <View style={styles.container}>
+            <TouchableOpacity
+                style={styles.closeButton}
+                onPress={() => navigation.navigate("Leaderboard")}
+            >
+                <Text style={styles.closeButtonText}>X</Text>
+            </TouchableOpacity>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    closeButton: {
+        position: "absolute",
+        top: 20,
+        right: 20,
+        zIndex: 10,
+    },
+    closeButtonText: {
+        fontSize: 24,
+        color: "black",
+    },
+});
