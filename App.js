5c97e655c94dc3af0048d590dd64cc0a2363bdd7
Nathan Hartojo <nathanhartojo@gmail.com>
2024-02-24 18:16:58 -0600

    basic routing

--- App.js
+++ App.js
@@ -1,108 +1,54 @@
-import { useState } from "react";
-import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { StyleSheet, View } from "react-native";
-import * as ImagePicker from "expo-image-picker";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons as Icon } from "@expo/vector-icons";
 
-import Button from "./components/Button";
-import ImageViewer from "./components/ImageViewer";
-import CircleButton from "./components/CircleButton";
-import IconButton from "./components/IconButton";
-import EmojiPicker from "./components/EmojiPicker";
-import EmojiList from "./components/EmojiList";
-import EmojiSticker from "./components/EmojiSticker";
+import CameraDrowsy from "./components/CameraDrowsy";
+import SettingScreen from "./components/SettingScreen";
+import Leaderboard from "./components/Leaderboard";
 
-const PlaceholderImage = require("./assets/images/background-image.png");
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [showAppOptions, setShowAppOptions] = useState(false);
-    const [pickedEmoji, setPickedEmoji] = useState(null);
-    const [selectedImage, setSelectedImage] = useState(null);
-
-    const pickImageAsync = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            allowsEditing: true,
-            quality: 1,
-        });
-
-        if (!result.canceled) {
-            setSelectedImage(result.assets[0].uri);
-            setShowAppOptions(true);
-        } else {
-            alert("You did not select any image.");
-        }
-    };
-
-    const onReset = () => {
-        setShowAppOptions(false);
-    };
-
-    const onAddSticker = () => {
-        setIsModalVisible(true);
-    };
-
-    const onModalClose = () => {
-        setIsModalVisible(false);
-    };
-
-    const onSaveImageAsync = async () => {
-        // we will implement this later
-    };
-
     return (
         <SafeAreaProvider>
-            <GestureHandlerRootView style={styles.container}>
-                <View style={styles.imageContainer}>
-                    <ImageViewer
-                        placeholderImageSource={PlaceholderImage}
-                        selectedImage={selectedImage}
-                    />
-                    {pickedEmoji !== null ? (
-                        <EmojiSticker
-                            imageSize={40}
-                            stickerSource={pickedEmoji}
-                        />
-                    ) : null}
-                </View>
-                {showAppOptions ? (
-                    <View style={styles.optionsContainer}>
-                        <View style={styles.optionsRow}>
-                            <IconButton
-                                icon="refresh"
-                                label="Reset"
-                                onPress={onReset}
-                            />
-                            <CircleButton onPress={onAddSticker} />
-                            <IconButton
-                                icon="save-alt"
-                                label="Save"
-                                onPress={onSaveImageAsync}
-                            />
-                        </View>
-                    </View>
-                ) : (
-                    <View style={styles.footerContainer}>
-                        <Button
-                            theme="primary"
-                            label="Choose a photo"
-                            onPress={pickImageAsync}
-                        />
-                        <Button
-                            label="Use this photo"
-                            onPress={() => setShowAppOptions(true)}
-                        />
-                    </View>
-                )}
-                <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-                    <EmojiList
-                        onSelect={setPickedEmoji}
-                        onCloseModal={onModalClose}
-                    />
-                </EmojiPicker>
-                <StatusBar style="light" />
-            </GestureHandlerRootView>
+            <NavigationContainer>
+                <Tab.Navigator
+                    screenOptions={({ route }) => ({
+                        tabBarIcon: ({ focused, color, size }) => {
+                            let iconName;
+
+                            if (route.name === "Camera") {
+                                iconName = focused
+                                    ? "camera"
+                                    : "camera-outline";
+                            } else if (route.name === "Leaderboard") {
+                                iconName = focused
+                                    ? "trophy"
+                                    : "trophy-outline";
+                            } else if (route.name === "Settings") {
+                                iconName = focused ? "cog" : "cog-outline";
+                            }
+                            return (
+                                <Icon
+                                    name={iconName}
+                                    size={size}
+                                    color={color}
+                                />
+                            );
+                        },
+                        tabBarActiveTintColor: "tomato",
+                        tabBarInactiveTintColor: "gray",
+                        tabBarStyle: { backgroundColor: "#25292e" },
+                    })}
+                >
+                    <Tab.Screen name="Camera" component={CameraDrowsy} />
+                    <Tab.Screen name="Leaderboard" component={Leaderboard} />
+                    <Tab.Screen name="Settings" component={SettingScreen} />
+                </Tab.Navigator>
+            </NavigationContainer>
         </SafeAreaProvider>
     );
 }
