dd72472dc18a8c7a8849d7c657f11abed5f7f10d
Nathan Hartojo <nathanhartojo@gmail.com>
2024-02-24 18:24:52 -0600

    add comp

---
+++ components/CameraDrowsy.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function CameraDrowsy() {
+    return (
+        <View>
+            <Text>We made it</Text>
+        </View>
+    );
+}
---
+++ components/Leaderboard.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Leaderboard() {
+    return (
+        <View>
+            <Text>We made it to the leaderboard</Text>
+        </View>
+    );
+}
